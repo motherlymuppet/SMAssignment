55b440a12426441f20ca3a4e13133d0186d4554a
Steven Lowes <motherlymuppet@users.noreply.github.com>
2017-11-14 09:05:40 +0000

    Make infinity random work from pheremones array
    TODO make sure weightedRandom() works all the time - rounding errors in doubles!

--- src/main/java/org/stevenlowes/university/softwaremethodologies/aisearch/FastSquareArray.kt
+++ src/main/java/org/stevenlowes/university/softwaremethodologies/aisearch/FastSquareArray.kt
@@ -1,10 +1,16 @@
 package org.stevenlowes.university.softwaremethodologies.aisearch
 
+import java.util.*
+
 open class FastSquareArray(val size: Int, val initialiser: (Int, Int) -> Float){
     val array: FloatArray = FloatArray(size * size, init = {initialiser(it%size, it/size)})
 
     fun get(x: Int, y: Int): Float{
         return array[x * size + y]
+    }
+
+    companion object {
+        val rand = Random()
     }
 
     fun set(x: Int, y: Int, value: Float){
@@ -33,6 +39,29 @@ open class FastSquareArray(val size: Int, val initialiser: (Int, Int) -> Float){
         val startIndex = x * size
         val endIndex = (x + 1) * size
         return array.copyOfRange(startIndex, endIndex)
+    }
+
+    fun weightedRandom(x: Int, options: List<Int>): Int {
+        /*
+        if(options.size == 1){
+            return options.first()
+        }
+        */
+
+        val max = options.sumByDouble { get(x, it).toDouble() }
+        val random = rand.nextDouble() * max
+
+        var runningTotal = 0f
+        for (y in options) {
+            val value = get(x, y)
+            runningTotal += value
+            if (runningTotal >= random) {
+                if (x != y) {
+                    return y
+                }
+            }
+        }
+        throw RuntimeException("This should never happen")
     }
 
     val average get() = array.average().toFloat()
