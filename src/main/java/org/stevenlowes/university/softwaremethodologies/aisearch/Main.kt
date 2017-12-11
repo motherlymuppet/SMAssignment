690dc288e46d8b87529a500ebaee5e8d4e9fcc1e
Steven Lowes <motherlymuppet@users.noreply.github.com>
2017-12-11 08:33:56 +0000

    Work on report

--- src/main/java/org/stevenlowes/university/softwaremethodologies/aisearch/Main.kt
+++ src/main/java/org/stevenlowes/university/softwaremethodologies/aisearch/Main.kt
@@ -10,8 +10,8 @@ import org.stevenlowes.university.softwaremethodologies.aisearch.multilevel.solv
 
 fun main2(args: Array<String>) {
     val rootLevel = TextParser.parseFile("testfiles/10.txt")
-    val solver = SimulatedAnnealingSolver(70.0, 1.0, 2.5, 400, 0, 100*1000)
-    //val solver = AntColonySolver(100 * 1000, 2.0, 2.0, 0.2f, 0.6f, 1f, 0f, 25, 0)
+    //val solver = SimulatedAnnealingSolver(70.0, 1.0, 2.5, 400, 0, 100*1000)
+    val solver = AntColonySolver(128,2.0, 2.0, 0.2f, 0.8f, 1f, 0.0001f, 100, 0)
     val grouper = SimpleGrouper()
     //val grouper = EnergeticGrouper(3f, 15)
     val matrix = Matrix(solver, grouper)
@@ -20,7 +20,7 @@ fun main2(args: Array<String>) {
     val startTime = System.nanoTime()
     val path = matrix.solve()
     val endTime = System.nanoTime()
-    println(path)
+    //println(path)
     println(path.distance)
     println("Done!")
     val dif = (endTime - startTime).toDouble() / 1000 / 1000 / 1000
