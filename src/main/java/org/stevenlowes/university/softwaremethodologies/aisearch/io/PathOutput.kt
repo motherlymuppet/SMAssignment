31a4abbe2a1c09f9503c314ea80fcb30ed6fa331
Steven Lowes <motherlymuppet@users.noreply.github.com>
2017-12-14 20:02:41 +0000

    Fix output format

--- src/main/java/org/stevenlowes/university/softwaremethodologies/aisearch/io/PathOutput.kt
+++ src/main/java/org/stevenlowes/university/softwaremethodologies/aisearch/io/PathOutput.kt
@@ -6,13 +6,13 @@ class PathOutput() {
     companion object {
         fun output(path: Path, name: String) {
             val length = path.nodes.size
-            val distance = path.distance
+            val distance = path.distance.toInt()
             val nodes = path.nodes.joinToString(",") { (it.id + 1).toString() }
 
             PrintWriter("tour$name.txt").use {
                 it.println("NAME = $name,")
                 it.println("TOURSIZE = $length,")
-                it.println("LENGTH = $distance")
+                it.println("LENGTH = $distance,")
                 it.println(nodes)
             }
         }
